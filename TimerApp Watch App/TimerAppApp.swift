2c2396ec6893ca58e01860581c56278278d45434
Jake Huang <zijianhuang03@gmail.com>
2025-01-25 19:59:32 -0500

    Initial Commit

---
+++ TimerApp Watch App/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp Watch App
+//
+//  Created by Jake Huang on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerApp Watch AppTests/TimerApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp_Watch_AppTests.swift
+//  TimerApp Watch AppTests
+//
+//  Created by Jake Huang on 1/25/25.
+//
+
+import Testing
+@testable import TimerApp_Watch_App
+
+struct TimerApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
